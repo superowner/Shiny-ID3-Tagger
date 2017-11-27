1f95a96cddb7a2828c10a03734336ea021af5666
shinyid3tagger <shinyid3tagger@gmail.com>
2017-11-27 23:39:46 +0100

    - store access token for later reuse

--- code/Webservices/ID3Tags/GetTags_Spotify.cs
+++ code/Webservices/ID3Tags/GetTags_Spotify.cs
@@ -36,60 +36,69 @@ namespace GlobalNamespace
 			string artistEnc = WebUtility.UrlEncode(artist);
 			string titleEnc = WebUtility.UrlEncode(title);
 			
-			HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+			HttpRequestMessage request = new HttpRequestMessage();
 			
-			Dictionary<string, string> postBody = new Dictionary<string, string> { { "grant_type", "client_credentials" } };			
-			FormUrlEncodedContent postBodyEnc = new FormUrlEncodedContent(postBody);
-			request.Content = postBodyEnc;
+			if (SessionData.SpAccessToken == null || SessionData.SpAccessTokenExpireDate < DateTime.Now)
+			{			
+				request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+				
+				Dictionary<string, string> postBody = new Dictionary<string, string> { { "grant_type", "client_credentials" } };			
+				FormUrlEncodedContent postBodyEnc = new FormUrlEncodedContent(postBody);
+				request.Content = postBodyEnc;
+		
+				string credidsPlain = User.Accounts["SpClientId"] + ":" + User.Accounts["SpClientSecret"];
+				byte[] credidsBytes = Encoding.UTF8.GetBytes(credidsPlain);
+				string creditsBase64 = Convert.ToBase64String(credidsBytes);
+				request.Headers.Add("Authorization", "Basic " + creditsBase64);
 	
-			string credidsPlain = User.Accounts["SpClientId"] + ":" + User.Accounts["SpClientSecret"];
-			byte[] credidsBytes = Encoding.UTF8.GetBytes(credidsPlain);
-			string creditsBase64 = Convert.ToBase64String(credidsBytes);
-			request.Headers.Add("Authorization", "Basic " + creditsBase64);
+				string loginContent = await this.GetRequest(client, request, cancelToken);
+				JObject loginData = JsonConvert.DeserializeObject<JObject>(loginContent, this.GetJsonSettings());
 
-			string content1 = await this.GetRequest(client, request, cancelToken);
-			JObject data1 = JsonConvert.DeserializeObject<JObject>(content1, this.GetJsonSettings());
+				if (loginData != null && loginData.SelectToken("access_token") != null)
+				{
+					SessionData.SpAccessToken = (string)loginData.SelectToken("access_token");
+					TimeSpan validDuration = TimeSpan.FromSeconds((int)loginData.SelectToken("expires_in"));
+					SessionData.SpAccessTokenExpireDate = DateTime.Now.Add(validDuration);					
+				}				
+			}
 			
-			if (data1 != null && data1.SelectToken("access_token") != null)
+			if (SessionData.SpAccessToken != null)
 			{
 				request = new HttpRequestMessage();
 				request.RequestUri = new Uri("https://api.spotify.com/v1/search?q=artist:\"" + artistEnc + "\" title:\"" + titleEnc + "\"&type=track&limit=1");
-				request.Headers.Add("Authorization", "Bearer " + data1.SelectToken("access_token"));
+				request.Headers.Add("Authorization", "Bearer " + SessionData.SpAccessToken);
 	
-				string content2 = await this.GetRequest(client, request, cancelToken);
-				JObject data2 = JsonConvert.DeserializeObject<JObject>(content2, this.GetJsonSettings());
+				string content1 = await this.GetRequest(client, request, cancelToken);
+				JObject data1 = JsonConvert.DeserializeObject<JObject>(content1, this.GetJsonSettings());
 	
-				if (data2 != null && data2.SelectToken("tracks.items") != null && data2.SelectToken("tracks.items").Any())
+				if (data1 != null && data1.SelectToken("tracks.items") != null && data1.SelectToken("tracks.items").Any())
 				{
-					o.Artist = (string)data2.SelectToken("tracks.items[0].artists[0].name");
-					o.Title = (string)data2.SelectToken("tracks.items[0].name");
-					o.Album = (string)data2.SelectToken("tracks.items[0].album.name");
-					o.DiscNumber = (string)data2.SelectToken("tracks.items[0].disc_number");
-					o.TrackNumber = (string)data2.SelectToken("tracks.items[0].track_number");
+					o.Artist = (string)data1.SelectToken("tracks.items[0].artists[0].name");
+					o.Title = (string)data1.SelectToken("tracks.items[0].name");
+					o.Album = (string)data1.SelectToken("tracks.items[0].album.name");
+					o.DiscNumber = (string)data1.SelectToken("tracks.items[0].disc_number");
+					o.TrackNumber = (string)data1.SelectToken("tracks.items[0].track_number");
 	
 					// ###########################################################################
 					request = new HttpRequestMessage();
-					if (data1 != null && data1.SelectToken("access_token") != null)
-					{
-						request.Headers.Add("Authorization", "Bearer " + data1.SelectToken("access_token"));
-					}
+					request.Headers.Add("Authorization", "Bearer " + SessionData.SpAccessToken);
 					
-					string url = (string)data2.SelectToken("tracks.items[0].album.href");
+					string url = (string)data1.SelectToken("tracks.items[0].album.href");
 					if (IsValidUrl(url))
 					{
 						request.RequestUri = new Uri(url); 
-					}
 					
-					string content3 = await this.GetRequest(client, request, cancelToken);
-					JObject data3 = JsonConvert.DeserializeObject<JObject>(content3, this.GetJsonSettings());
-	
-					if (data3 != null)
-					{
-						o.Date = (string)data3.SelectToken("release_date");
-						o.DiscCount = (string)data3.SelectToken("tracks.items[-1:].disc_number");
-						o.TrackCount = (string)data3.SelectToken("tracks.total");
-						o.Genre = string.Join(", ", data3.SelectToken("genres"));		// Always empty even for full album lookups. Seems like a general spotify issue: https://github.com/spotify/web-api/issues/157				
-						o.Cover = (string)data3.SelectToken("images").OrderBy(obj => obj["height"]).Last()["url"];
+						string content2 = await this.GetRequest(client, request, cancelToken);
+						JObject data2 = JsonConvert.DeserializeObject<JObject>(content2, this.GetJsonSettings());
+		
+						if (data2 != null)
+						{
+							o.Date = (string)data2.SelectToken("release_date");
+							o.DiscCount = (string)data2.SelectToken("tracks.items[-1:].disc_number");
+							o.TrackCount = (string)data2.SelectToken("tracks.total");
+							o.Genre = string.Join(", ", data2.SelectToken("genres"));		// genres is always empty even for full album lookups. Seems like a general spotify issue: https://github.com/spotify/web-api/issues/157				
+							o.Cover = (string)data2.SelectToken("images").OrderBy(obj => obj["height"]).Last()["url"];
+						}
 					}
 				}
 			}
