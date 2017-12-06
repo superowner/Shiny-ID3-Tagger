65977010a5f0bb3b4459a9b51815a9a203656235
shinyid3tagger <shinyid3tagger@gmail.com>
2017-12-06 19:00:16 +0100

    - fixed null reference exception

--- source code/API/ID3/GetTags_Discogs.cs
+++ source code/API/ID3/GetTags_Discogs.cs
@@ -48,44 +48,46 @@ namespace GlobalNamespace
 				string searchContent = await this.GetResponse(client, searchRequest, cancelToken);
 				JObject searchData = JsonConvert.DeserializeObject<JObject>(searchContent, this.GetJsonSettings());
 
-				string albumUrl = (string)searchData.SelectToken("results[0].resource_url");
-
-				if (searchData != null && searchData.SelectToken("results").Any() && IsValidUrl(albumUrl))
+				if (searchData != null && searchData.SelectToken("results").Any())
 				{
-					using (HttpRequestMessage albumRequest = new HttpRequestMessage())
+					string albumUrl = (string)searchData.SelectToken("results[0].resource_url");
+					if (albumUrl != null && IsValidUrl(albumUrl))
 					{
-						albumRequest.Headers.Add("User-Agent", User.Settings["UserAgent"]);
-						albumRequest.RequestUri = new Uri(albumUrl + "?key=" + User.Accounts["DcKey"] + "&secret=" + User.Accounts["DcSecret"]);
-
-						string albumContent = await this.GetResponse(client, albumRequest, cancelToken);
-						JObject albumData = JsonConvert.DeserializeObject<JObject>(albumContent, this.GetJsonSettings());
-
-						if (albumData != null)
+						using (HttpRequestMessage albumRequest = new HttpRequestMessage())
 						{
-							o.Artist = (string)albumData.SelectToken("artists[0].name");
-							o.Title = null;
-							o.Album = (string)albumData.SelectToken("title");
-							o.Date = (string)albumData.SelectToken("year");
-							o.Genre = (string)albumData.SelectToken("genres[0]");
-							o.DiscCount = null;
-							o.DiscNumber = null;
-							o.Cover = (string)albumData.SelectToken("images[0].uri");
+							albumRequest.Headers.Add("User-Agent", User.Settings["UserAgent"]);
+							albumRequest.RequestUri = new Uri(albumUrl + "?key=" + User.Accounts["DcKey"] + "&secret=" + User.Accounts["DcSecret"]);
 
-							o.TrackCount = albumData.SelectTokens("tracklist[*]")
-								.Where(t => t.SelectToken("type_").ToString().ToLowerInvariant() == "track")
-								.ToArray().Length.ToString();
+							string albumContent = await this.GetResponse(client, albumRequest, cancelToken);
+							JObject albumData = JsonConvert.DeserializeObject<JObject>(albumContent, this.GetJsonSettings());
 
-							// This API is strange. You can either search for a release and don't get the album. Or you search for the album but don't get the release
-							// You can do one search with "format=album" to maybe get the album and a second search without "format=album" to maybe get the single and therefore the track title
-							// But no one will guarantee that the second search shows a title which is on your album from the first search
-							// How can I get a response which holds the album and title at the same time?
-							// Currently I'm just checking if the album track list contains a title which equals the initial title (from filename or ID3 tag)
-							JToken[] tracklist = albumData.SelectTokens("tracklist[*].title").ToArray();
-							int temp = Array.FindIndex(tracklist, t => t.ToString().ToLowerInvariant() == title.ToLowerInvariant());
-							if (temp != -1)
+							if (albumData != null)
 							{
-								o.Title = title;
-								o.TrackNumber = (temp + 1).ToString();
+								o.Artist = (string)albumData.SelectToken("artists[0].name");
+								o.Title = null;
+								o.Album = (string)albumData.SelectToken("title");
+								o.Date = (string)albumData.SelectToken("year");
+								o.Genre = (string)albumData.SelectToken("genres[0]");
+								o.DiscCount = null;
+								o.DiscNumber = null;
+								o.Cover = (string)albumData.SelectToken("images[0].uri");
+
+								o.TrackCount = albumData.SelectTokens("tracklist[*]")
+									.Where(t => t.SelectToken("type_").ToString().ToLowerInvariant() == "track")
+									.ToArray().Length.ToString();
+
+								// This API is strange. You can either search for a release and don't get the album. Or you search for the album but don't get the release
+								// You can do one search with "format=album" to maybe get the album and a second search without "format=album" to maybe get the single and therefore the track title
+								// But no one will guarantee that the second search shows a title which is on your album from the first search
+								// How can I get a response which holds the album and title at the same time?
+								// Currently I'm just checking if the album track list contains a title which equals the initial title (from filename or ID3 tag)
+								JToken[] tracklist = albumData.SelectTokens("tracklist[*].title").ToArray();
+								int temp = Array.FindIndex(tracklist, t => t.ToString().ToLowerInvariant() == title.ToLowerInvariant());
+								if (temp != -1)
+								{
+									o.Title = title;
+									o.TrackNumber = (temp + 1).ToString();
+								}
 							}
 						}
 					}
